686ff3138a8891ece99f68328383b4b5ea451854
Jian Wang <wangluojisuan@163.com>
2017-08-04 07:33:35 +0800

    Linux建立idea项目

---
+++ ch03/testproject01/src/com/jw/testproject01/Main.java
@@ -0,0 +1,9 @@
+package com.jw.testproject01;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("TestProject01");
+    }
+}
