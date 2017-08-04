9e71c1db914ba01e0a592d7f4b02630f3c4f23a1
WangJ <wangluojisuan@163.com>
2017-08-04 23:45:17 +0800

    添加测试工程

---
+++ Test/TestProject01/tests/com/jw/tp01/MainTest.java
@@ -0,0 +1,17 @@
+package com.jw.tp01;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void main() throws Exception {
+    }
+
+}
