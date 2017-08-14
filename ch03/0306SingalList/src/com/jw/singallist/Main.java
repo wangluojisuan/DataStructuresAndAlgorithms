401a97a681e0e81a04b5a09ef1f7410a37709a27
Jian Wang <wangluojisuan@163.com>
2017-08-14 14:39:19 +0800

    添加insert

--- ch03/0306SingalList/src/com/jw/singallist/Main.java
+++ ch03/0306SingalList/src/com/jw/singallist/Main.java
@@ -5,5 +5,11 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("单链表的实现");
+        SingalList singalList = new SingalList(new ListNode(10));
+        singalList.insert(new ListNode(9),0);
+        singalList.insert(new ListNode(8),0);
+        singalList.insert(new ListNode(7),0);
+        System.out.println(singalList.getLength());
+        System.out.println(singalList.toString());
     }
 }
--- ch03/0306SingalList/src/com/jw/singallist/SingalList.java
+++ ch03/0306SingalList/src/com/jw/singallist/SingalList.java
@@ -12,10 +12,44 @@ public class SingalList {
         this.len = 1;
     }
 
+    /**
+     * 获取链表的长度
+     *
+     * @return
+     */
     public int getLength() {
         return this.len;
     }
 
+    /**
+     * 插入节点
+     * @param node 节点
+     * @param pos 位置
+     */
+    public void insert(ListNode node, int pos) {
+        if (pos < this.len) {
+            ListNode currentNode = this.head;
+            for (int i = 0; i < pos; i++) {
+                currentNode = currentNode.getNext();
+            }
+            node.setNext(currentNode.getNext());
+            currentNode.setNext(node);
+            this.len += 1;
+        } else {
+            ListNode currentNode = this.head;
+            while (currentNode.getNext() != null) {
+                currentNode = currentNode.getNext();
+            }
+            node.setNext(null);
+            currentNode.setNext(node);
+        }
+    }
+
+    /**
+     * 输出链表的内容
+     *
+     * @return
+     */
     @Override
     public String toString() {
         StringBuilder strSingalList = new StringBuilder();
@@ -23,7 +57,7 @@ public class SingalList {
         ListNode currentNode = this.head;
 
         while (currentNode.getNext() != null) {
-            strSingalList.append(currentNode.getData() + "->");
+            strSingalList.append(currentNode.getNext().getData() + "->");
             currentNode = currentNode.getNext();
         }
         strSingalList = strSingalList.delete(strSingalList.length() - 2, strSingalList.length());
