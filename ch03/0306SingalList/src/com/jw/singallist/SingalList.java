afee016891e834fe423e5450141b15f43e1e77c5
Jian Wang <wangluojisuan@163.com>
2017-08-21 09:24:08 +0800

    添加链表删除操作

--- ch03/0306SingalList/src/com/jw/singallist/SingalList.java
+++ ch03/0306SingalList/src/com/jw/singallist/SingalList.java
@@ -48,7 +48,7 @@ public class SingalList {
     /**
      * 输出链表的内容
      *
-     * @return
+     * @return 链表的内容
      */
     @Override
     public String toString() {
@@ -63,5 +63,44 @@ public class SingalList {
         strSingalList = strSingalList.delete(strSingalList.length() - 2, strSingalList.length());
 
         return strSingalList.toString();
+    }
+
+    /**
+     * 删除指定位置的链表节点
+     * @param pos 位置
+     * @return  执行成功或失败
+     */
+    public Boolean delete(int pos){
+        if(this.len<=0){
+            System.out.println("空链表，不能继续删除");
+            return false;
+        }
+
+        if(pos<=0){
+            this.head.setNext(this.head.getNext().getNext());   // 获取下一个节点的下一个，由于Java自动管理内存，所以不需要释放内存
+            this.len -= 1;
+            return true;
+        }else if(pos>=(this.len-1)){
+            ListNode currentNode = this.head;
+            while (currentNode.getNext().getNext() != null){
+               currentNode.setNext(currentNode.getNext());
+            }
+            currentNode.setNext(null);
+            this.len -= 1;
+            return true;
+        }else {
+            ListNode currentNode = this.head;
+            int index = 0;
+            while (currentNode.getNext().getNext() != null){
+                currentNode.setNext(currentNode.getNext());
+                index++;
+                if(index>=pos){
+                    break;
+                }
+            }
+            currentNode.setNext(currentNode.getNext().getNext());
+            this.len -= 1;
+            return true;
+        }
     }
 }
