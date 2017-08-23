f9003ac65e78bf6b756a76fb19da4ec294b60c17
Jian Wang <wangluojisuan@163.com>
2017-08-23 09:05:26 +0800

    添加动态数组实现栈的框架

---
+++ ch04/040602DynamicAarrayStack/src/com/jw/stack/DynamicArrayStack.java
@@ -0,0 +1,65 @@
+package com.jw.stack;
+
+/**
+ * 动态数组实现的栈，栈满之后数组容量增大一倍，
+ * 原有数据复制到新数组中去。
+ */
+public class DynamicArrayStack {
+
+    // 方法
+
+    /**
+     * 压入元素
+     * @param data
+     */
+    public void push(int data){
+
+    }
+
+    /**
+     * 弹出元素
+     * @return
+     */
+    public int pop(){
+
+    }
+
+    /**
+     * 最后压入的元素
+     * @return
+     */
+    public int top(){
+
+    }
+
+    /**
+     * 栈大小
+     * @return
+     */
+    public int size(){
+
+    }
+
+    /**
+     * 是否空栈
+     * @return
+     */
+    public Boolean isEmpty(){
+
+    }
+
+    /**
+     * 是否满栈
+     * @return
+     */
+    public Boolean isFull(){
+
+    }
+
+    /**
+     * 清空栈
+     */
+    public void clear(){
+
+    }
+}
---
+++ ch04/040602DynamicAarrayStack/src/com/jw/stack/Main.java
@@ -0,0 +1,8 @@
+package com.jw.stack;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
